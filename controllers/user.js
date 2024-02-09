d5cf040b260bfaf846d6bcf4c8fc652ed670d2d5
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-02-09 22:15:08 +0300

    почти готовый бэк

--- controllers/user.js
+++ controllers/user.js
@@ -3,8 +3,8 @@
 /* eslint-disable object-property-newline */
 /* eslint-disable max-len */
 // eslint-disable-next-line import/no-unresolved
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 const HttpCodes = require('../constants/constants');
