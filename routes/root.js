816248497a9b2530681e8420f4d69b7ecaef8895
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-02-17 18:18:43 +0300

    почти готовый бэк

--- routes/root.js
+++ routes/root.js
@@ -6,6 +6,7 @@ const auth = require('../middlewares/auth');
 const { loginValidation, registerValidation } = require('../joi/userValidation');
 
 router.post('/signin', loginValidation, login);
+console.log(login);
 router.post('/signup', registerValidation, createUser);
 router.use(auth);
 router.use('/users', userRouter);
